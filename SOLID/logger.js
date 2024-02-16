ac789fe6d3173eb2c71a6c4d57f1f4c7c18058f1
Zaki <reborn7875@gmail.com>
2024-02-16 18:37:31 +0800

    docs: learn SOLID S by javascript

---
+++ SOLID/logger.js
@@ -0,0 +1,7 @@
+function logMessage(message) {
+  console.log(message);
+  //   can be emailed user or other function
+  //   the reason is to make it easier to read
+}
+
+module.exports = logMessage;
